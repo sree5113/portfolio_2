2b573e4acd49701f0972030e198fc35a8f812195
sree5113 <vsreenivasareddyvasu@gmail.com>
2025-01-02 15:54:24 +0530

    dt

---
+++ p.js
@@ -0,0 +1,15 @@
+setInterval(function(){
+    let ct = new Date().toLocaleTimeString()
+    document.getElementsByClassName('span')[0].innerHTML=ct
+},1000)
+
+
+
+setInterval(function(){
+    let ct = new Date().toLocaleDateString()
+    document.getElementsByClassName('span')[1].innerHTML=ct
+},1000)
+
+
+
+
